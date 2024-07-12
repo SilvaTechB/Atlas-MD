8d2a01d48a9931cbe30736ac61ca3090c65a6e75
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-07-12 08:34:15 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "917086726371";   // You can replace this number with yours //
 }
 
 
-global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
-global.sessionId = process.env.SESSION_ID || "ok";
+global.owner = gg.split("917086726371");
+global.mongodb = process.env.MONGODB || "mongodb+srv://Mango:Mango12345@cluster0.hbfcpld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.sessionId = process.env.SESSION_ID || "kumi";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
